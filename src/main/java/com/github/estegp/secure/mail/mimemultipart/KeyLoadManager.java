ad61efcaab36fbe3fae786629ea4119f5690d2a2
estefaniagPerez <este.gutierrez.perez@gmail.com>
2025-12-24 19:10:56 +0100

    chore: dependencies vulnerability testing with trivy

--- src/main/java/com/github/estegp/secure/mail/mimemultipart/KeyLoadManager.java
+++ src/main/java/com/github/estegp/secure/mail/mimemultipart/KeyLoadManager.java
@@ -3,7 +3,6 @@ package com.github.estegp.secure.mail.mimemultipart;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.MessageDigest;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.cert.Certificate;
@@ -89,8 +88,10 @@ public enum KeyLoadManager {
   private PGPEncryptedDataGenerator iniEncryptorPgp(PGPPublicKey key) {
 
     PGPEncryptedDataGenerator encGen =
-        new PGPEncryptedDataGenerator(new JcePGPDataEncryptorBuilder(PGPEncryptedData.CAST5)
-            .setSecureRandom(new SecureRandom()).setProvider("BC"));
+        new PGPEncryptedDataGenerator(
+            new JcePGPDataEncryptorBuilder(PGPEncryptedData.CAST5)
+                .setSecureRandom(new SecureRandom())
+                .setProvider("BC"));
     encGen.addMethod(new JcePublicKeyKeyEncryptionMethodGenerator(key).setProvider("BC"));
 
     return encGen;
@@ -109,8 +110,9 @@ public enum KeyLoadManager {
     // ByteArrayOutputStream Close() method does nothing, no need to call it
 
     // Initializes reader
-    PGPPublicKeyRingCollection pgpPub = new PGPPublicKeyRingCollection(
-        PGPUtil.getDecoderStream(keyIn), new JcaKeyFingerprintCalculator());
+    PGPPublicKeyRingCollection pgpPub =
+        new PGPPublicKeyRingCollection(
+            PGPUtil.getDecoderStream(keyIn), new JcaKeyFingerprintCalculator());
 
     // Reads each key in the input stream until it finds the encryption key
     Iterator<PGPPublicKeyRing> keyRingIter = pgpPub.getKeyRings();
@@ -129,24 +131,5 @@ public enum KeyLoadManager {
     }
 
     throw new IllegalArgumentException("Can't find encryption key in key ring.");
-  }
-
-  /**
-   * TEST METHOD TO TRIGGER SECURITY VULNERABILITIES DETECTION
-   *
-   * @throws Exception
-   */
-  public void testVulnerability() throws Exception {
-    // TRIGGER: Weak Hashing Algorithm (MD5)
-    MessageDigest md = MessageDigest.getInstance("MD5");
-    byte[] hash = md.digest("password".getBytes());
-
-    // TRIGGER: Hardcoded Password
-    String secret = "superSecretPassword123";
-
-    // TRIGGER: Weak Random Number Generator
-    java.util.Random random = new java.util.Random();
-    int key = random.nextInt();
-    System.out.println("Hash: " + hash + " Secrets: " + secret + " Key: " + key);
   }
 }
